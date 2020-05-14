a646b6773d131184424440886de60296245d93e3
TheQwertyMan <62036209+TheQwertyMan@users.noreply.github.com>
2020-05-14 17:17:49 +0200

    Improved script
    Made it easier to add words/change message when someone asks

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-//https://discordapp.com/oauth2/authorize?client_id=[Your client id here]&scope=bot
+//https://discordapp.com/oauth2/authorize?client_id=[your token id here]&scope=bot
 //nodemon --inspect index.js
 
 //Instructions
-//First go to this site: https://discordapp.com/oauth2/authorize?client_id=[Your client id here]&scope=bot
+//First go to this site: https://discordapp.com/oauth2/authorize?client_id=[your token id here]&scope=bot
 //Select the Mirror House Server and click on 'Authorize'
 //Complete the captcha
 //Now the bot is added, if anyone types these keywords and is not a 'Pure Ones', the bot will send one of these responses: WE DONT HAVE ANY OF THE LEAKS YOU ASK FOR - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA> or THERE ARE ONLY LEAKS IF ITS ON #leak-tracker - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>
@@ -18,17 +18,14 @@
 //mario64 proto
 //mario64 prototype
 //mario 64 prototype
-//new leaks?
 //new leaks
-//new leak?
 //new leak
-
-//Preset
-// //
-// if (message.content.toLowerCase().includes('') && !message.member.roles.cache.some(role => role.name === 'Pure Ones')) {
-// message.reply('WE DONT HAVE ANY OF THE LEAKS YOU ASK FOR - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>'); 
-// }
-
+//where can i download
+//download link
+//2tb
+//2 tb
+//2terabyte
+//2 terabyte
 
 const Discord = require('discord.js');
  const client = new Discord.Client();
@@ -37,84 +34,50 @@ client.on('ready', () => {
  console.log(`Logged in as ${client.user.tag}!`);
  });
 
-var Mother = 'Mother'
-
 client.on('message', message => {
 	
- //	WE DONT HAVE ANY OF THE LEAKS YOU ASK FOR
+	//Debug Test
+	if (message.content === 'justworkgoddamn'){
+		message.reply('Calm down bro'); 
+	}
 	
- //Mother 64
- if (message.content.toLowerCase().includes('mother 64') && !message.member.roles.cache.some(role => role.name === 'Pure Ones')){
- message.reply('WE DONT HAVE ANY OF THE LEAKS YOU ASK FOR - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>'); 
- }
- 
- //Mother64
- if (message.content.toLowerCase().includes('mother64') && !message.member.roles.cache.some(role => role.name === 'Pure Ones')) {
- message.reply('WE DONT HAVE ANY OF THE LEAKS YOU ASK FOR - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>'); 
- }
- 
- //Ura Zelda
- if (message.content.toLowerCase().includes('ura zelda') && !message.member.roles.cache.some(role => role.name === 'Pure Ones')) {
- message.reply('WE DONT HAVE ANY OF THE LEAKS YOU ASK FOR - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>'); 
- }
- 
- //spaceworld
- if (message.content.toLowerCase().includes('spaceworld') && !message.member.roles.cache.some(role => role.name === 'Pure Ones')) {
- message.reply('WE DONT HAVE ANY OF THE LEAKS YOU ASK FOR - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>'); 
- }
- 
- //mario 64 beta
- if (message.content.toLowerCase().includes('mario 64 beta') && !message.member.roles.cache.some(role => role.name === 'Pure Ones')) {
- message.reply('WE DONT HAVE ANY OF THE LEAKS YOU ASK FOR - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>'); 
- }
- 
- //mario64 beta
- if (message.content.toLowerCase().includes('mario64 beta') && !message.member.roles.cache.some(role => role.name === 'Pure Ones')) {
- message.reply('WE DONT HAVE ANY OF THE LEAKS YOU ASK FOR - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>'); 
- }
- 
- //mario64 beta
- if (message.content.toLowerCase().includes('mario64 beta') && !message.member.roles.cache.some(role => role.name === 'Pure Ones')) {
- message.reply('WE DONT HAVE ANY OF THE LEAKS YOU ASK FOR - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>'); 
- }
- 
- //mario 64 proto
- if (message.content.toLowerCase().includes('mario 64 proto') && !message.member.roles.cache.some(role => role.name === 'Pure Ones')) {
- message.reply('WE DONT HAVE ANY OF THE LEAKS YOU ASK FOR - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>'); 
- }
- 
- //mario64 prototype
- if (message.content.toLowerCase().includes('mario64 prototype') && !message.member.roles.cache.some(role => role.name === 'Pure Ones')) {
- message.reply('WE DONT HAVE ANY OF THE LEAKS YOU ASK FOR - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>'); 
- }
- 
- //mario64 prototype
- if (message.content.toLowerCase().includes('mario64 prototype') && !message.member.roles.cache.some(role => role.name === 'Pure Ones')) {
- message.reply('WE DONT HAVE ANY OF THE LEAKS YOU ASK FOR - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>'); 
- }
- 
- // THERE ARE ONLY LEAKS IF ITS ON #leak-tracker
- 
- //New leak?
- if (message.content.toLowerCase().includes('new leak?') && !message.member.roles.cache.some(role => role.name === 'Pure Ones')) {
- message.reply('THERE ARE ONLY LEAKS IF ITS ON #leak-tracker - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>'); 
- }
- 
- //new leak
- if (message.content.toLowerCase().includes('new leak') && !message.member.roles.cache.some(role => role.name === 'Pure Ones')) {
- message.reply('THERE ARE ONLY LEAKS IF ITS ON #leak-tracker - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>'); 
- }
- 
- //new leaks?
- if (message.content.toLowerCase().includes('new leaks?') && !message.member.roles.cache.some(role => role.name === 'Pure Ones')) {
- message.reply('THERE ARE ONLY LEAKS IF ITS ON #leak-tracker - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>'); 
- }
- 
- //new leaks
- if (message.content.toLowerCase().includes('new leaks') && !message.member.roles.cache.some(role => role.name === 'Pure Ones')) {
- message.reply('THERE ARE ONLY LEAKS IF ITS ON #leak-tracker - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>'); 
- }
- 
+	//Blacklists
+	let BlacklistDrive = ['2tb', '2 tb', '2terabyte', '2 terabyte']
+	let BlacklistTracker = ['new leaks', 'new leak']
+	let BlacklistSearch = ['where can i download', 'download link']
+	let BlacklistDontHave = ['ura zelda', 'mother 64', 'mother64', 'spaceworld', 'mario 64 beta', 'mario64 beta', 'mario 64 proto', 'mario64 proto', 'mario64 prototype',
+	'mario 64 prototype', ]
+	
+	//Blacklist filter Search
+	for (var i in BlacklistSearch) {
+		if (message.content.toLowerCase().includes(BlacklistSearch[i]) && !message.member.roles.cache.some(role => role.name === 'Pure Ones')) {
+			message.reply('JUST DO A GOOGLE SEARCH RETARD - TUTORIAL: <https://www.youtube.com/watch?v=5AFVQI8PsQA>');
+			break
+		}
+	}
+	
+	//Blacklist filter DontHave
+	for (var i in BlacklistDontHave) {
+		if (message.content.toLowerCase().includes(BlacklistDontHave[i]) && !message.member.roles.cache.some(role => role.name === 'Pure Ones')) {
+			message.reply('WE DONT HAVE ANY OF THE LEAKS YOU ASK FOR - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>');
+			break
+		}
+	}
+	
+	//Blacklist filter Tracker
+	for (var i in BlacklistTracker) {
+		if (message.content.toLowerCase().includes(BlacklistTracker[i]) && !message.member.roles.cache.some(role => role.name === 'Pure Ones')) {
+			message.reply('THERE ARE ONLY LEAKS IF ITS ON #leak-tracker - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>');
+			break
+		}
+	}
+
+	for (var i in BlacklistDrive) {
+		if (message.content.toLowerCase().includes(BlacklistDrive[i]) && !message.member.roles.cache.some(role => role.name === 'Pure Ones')) {
+			message.reply('THERE IS NO 2TB OF DATA - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>');
+			break
+		}
+	}	
 });
 
-client.login('Your Token Here');
+client.login('your token here');
