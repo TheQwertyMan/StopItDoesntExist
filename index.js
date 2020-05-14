7226f93919db794729ee8fdf2679503cb8e675d1
TheQwertyMan <62036209+TheQwertyMan@users.noreply.github.com>
2020-05-14 18:27:01 +0200

    Added Help
    Activate it with `i need help with the bot`

--- index.js
+++ index.js
@@ -30,6 +30,9 @@
 const Discord = require('discord.js');
  const client = new Discord.Client();
 
+//Timeout system variables
+var AmountofMessages = 0
+
 client.on('ready', () => {
  console.log(`Logged in as ${client.user.tag}!`);
  });
@@ -47,6 +50,19 @@ client.on('message', message => {
 	let BlacklistSearch = ['where can i download', 'download link']
 	let BlacklistDontHave = ['ura zelda', 'mother 64', 'mother64', 'spaceworld', 'mario 64 beta', 'mario64 beta', 'mario 64 proto', 'mario64 proto', 'mario64 prototype',
 	'mario 64 prototype', ]
+
+	//Helpbot
+	if (message.content === 'i need help with the bot'){
+		if (AmountofMessages === 0) {
+			message.reply('You asked for help, so here it is:\nThis bot says different thing if it detects that you said a keyword, the keywords at the time of writing it are:\n\n-WE DONT HAVE ANY OF THE LEAKS YOU ASK FOR - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>-\nUra Zelda, Mother 64, Mother64, Spaceworld, Mario 64 Beta, Mario64 Beta, Mario 64 Proto, Mario64 Proto, Mario 64 Prototype, Mario64 Prototype\n\n-THERE IS NO 2TB OF DATA - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>-\n2tb, 2 tb, 2Terabyte, 2 Terabyte\n\n-JUST DO A GOOGLE SEARCH RETARD - TUTORIAL: <https://www.youtube.com/watch?v=5AFVQI8PsQA>-\nWhere can i download, Download Link\n\n-THERE ARE ONLY LEAKS IF ITS ON <#708761059388948522> - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>-\nNew Leaks, New Leak\n\nThis bot was created by <@321965387048812544> and is open sourced: https://github.com/TheQwertyMan/StopItDoesntExist/blob/master/README.md');
+			AmountofMessages = 3
+		}
+		
+		else {
+			message.reply('There has already recently been asked for help, please use the searchbar');
+			--AmountofMessages
+		}
+	}	
 	
 	//Blacklist filter Search
 	for (var i in BlacklistSearch) {
