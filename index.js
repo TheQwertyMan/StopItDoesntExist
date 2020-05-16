7837368e23aae86b977d3247ef4bbc5b8d679e12
TheQwertyMan <62036209+TheQwertyMan@users.noreply.github.com>
2020-05-16 14:48:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 //nodemon --inspect index.js
 
+//Require stuff
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json')
 
-client.on('ready', () => {
- console.log(`${client.user.tag} Booted succesfully`);
- console.log('-Bot Log-')
- });
-
+//Startup messages
+client.once('ready', () => {
+	console.log(`${client.user.tag} Booted succesfully`)
+	console.log('-Bot Log-')
+});
+ 
 
 		
 //Blacklists
+let BlacklistCussWords = ['flake nude porn', 'flake nude', 'flake porn']
 let BlacklistBadBot = ['bad bot', 'bot bad', 'stupid bot', 'complaints about bot', 'fuck the bot', 'i hate this bot', 'remove bot', 'why tf did you make this bot', 'fuck this bot']
 let BlacklistDrive = ['2tb', '2 tb', '2terabyte', '2 terabyte', 'two terabyte', 'two tb']
 let BlacklistTracker = ['new leak', 'anything new', "what's new"]
@@ -23,7 +26,8 @@ let BlacklistDontHave = ['ura zelda', 'mother 64', 'mother64', 'mario 64 beta', 
 let AllStep1 = BlacklistBadBot.concat(BlacklistDrive)
 let AllStep2 = AllStep1.concat(BlacklistTracker)
 let AllStep3 = AllStep2.concat(BlacklistSearch)
-let All = AllStep3.concat (BlacklistDontHave)
+let AllStep4 = AllStep3.concat(BlacklistDontHave)
+let All = AllStep4.concat(BlacklistCussWords)
 
 
 //Variables
@@ -32,35 +36,45 @@ var messageBuffer = 'TempVal'
 var UsageCount = 0
 var Blacklisted = false
 
+//On message
 client.on('message', message => {
 	
+	//Check for an anonfiles link
 	if (message.content.toLowerCase().includes('anonfiles.com/')) {
-		//Remove the // if the bot has admin privliges
-		//message.remove()
 		message.reply('Please refrain from sending links to sites where there are known to be leaks')
+		//Remove the // if the bot has admin privliges
+		//message.delete()
 		console.log(`Deleted the message '${message.content}' by '${message.member.user.tag}' for includinig a link to a known filehoster with leaks`)
 	}
 	
-	//Test
+	//Give suggestions
+	if(message.content.toLowerCase().includes('suggestion:') && !Help) {
+		var word2 = message.content.toLowerCase().replace("suggestion: ", "");
+		message.reply ('Thanks for your suggestion')
+		console.log(`${word2} has been suggested by ${message.member.user.tag}`)
+	}
+
+	//Help = false
+	Help = false
+	
+	//Test words
 	if (message.content.toLowerCase().startsWith('test: ')) {
 		
-		++UsageCount
-		var word = message.content.toLowerCase().replace("test: ", ""); 
-		
+		var word = message.content.toLowerCase().replace("test: ", "");
 		for (var i in All) {		
 			if(message.content.toLowerCase().includes(All[i])) {
+				++UsageCount
 				message.reply (`'${word}' has already been blacklisted`)
 				console.log(`'${message.member.user.tag}' asked if '${word}' was blacklisted and it was '${All[i]}'`)
 				var Blacklisted = true
 				break
 			}
-		}
-		
+		}	
 		if (Blacklisted !== true) {
-			message.reply (`'${word}' has not been blacklisted, <@321965387048812544> has been notified on your suggestion`)
+			++UsageCount
+			message.reply (`'${word}' has not been blacklisted`)
 			console.log(`'${message.member.user.tag}' asked if '${word}' was blacklisted and it wasnt`)	
 		}
-		
 		Blacklisted = false
 		return
 	}
@@ -80,9 +94,10 @@ client.on('message', message => {
 	//Helpbot
 	if (message.content === 'i need help with the bot'){
 		if (AmountofMessages === 0) {
-			message.reply('You asked for help, so here it is:\nThis bot says different thing if it detects that you said a keyword, the keywords at the time of writing it are:\n\n-WE DONT HAVE ANY OF THE LEAKS YOU ASK FOR - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>-\nUra Zelda, Mother 64, Mother64, Spaceworld, Mario 64 Beta, Mario64 Beta, Mario 64 Proto, Mario64 Proto, Mario 64 Prototype, Mario64 Prototype\n\n-THERE IS NO 2TB OF DATA - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>-\n2tb, 2 tb, 2Terabyte, 2 Terabyte\n\n-JUST DO A GOOGLE SEARCH RETARD - TUTORIAL: <https://www.youtube.com/watch?v=5AFVQI8PsQA>-\nWhere can i download, Download Link, Where can i find\n\n-THERE ARE ONLY LEAKS IF ITS ON <#708761059388948522> - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>-\nNew Leaks, New Leak, Anything new\n\nIf you type `UsageCount` you can see how many times i have been summoned\nIf you type `Test: [Whateveryouwanttotest]` you can test if a word is blacklisted\n\nThis bot was created by <@321965387048812544> and is open sourced: https://github.com/TheQwertyMan/StopItDoesntExist');
+			message.reply(`you asked for help, so here it is:\n\nI filter through messages on this server in search of specific keywords\nYou can find the list here: <https://pastebin.com/vSCSN7eX>\n*This list might be outdated if i don't feel like updating it\n\nAvailable commands:\n- Test: [Word that you want to test] - Test for a word to see if it is blacklisted\n- Suggestion: [Word that you want to suggest] - Notifies <${config.UserToPing}> of your suggestion for new blacklisted words\n- UsageCount - Tells you how many times the bot has been used this session\n- i need help with the bot - Brings up this messages\n- justworkgoddamn - Simply tells you '<@user>, calm down bro'\n\nIf you have any questions, feel free to ping <${config.UserToPing}>\nThis bot was made by <@321965387048812544> and is open sourced: https://github.com/TheQwertyMan/StopItDoesntExist\n(The project is licenced under the GNU General Public License v3.0 licence)`);
 			AmountofMessages = 3
 			console.log(`${message.member.user.tag} requested help and got accepted`)
+			Help = true
 			++UsageCount
 		}
 		
@@ -112,6 +127,16 @@ client.on('message', message => {
 		if (message.content.toLowerCase().includes(BlacklistSearch[i]) && !message.member.roles.cache.some(role => role.name === 'Pure Ones')) {
 			message.reply('JUST DO A GOOGLE SEARCH RETARD - TUTORIAL: <https://www.youtube.com/watch?v=5AFVQI8PsQA>');
 			console.log(`'${message.member.user.tag}' said '${message.content}' and the bot found the keyword '${BlacklistSearch[i]}'`)			
+			++UsageCount
+			break
+		}
+	}
+	
+	//Blacklist filter Cusswords
+	for (var i in BlacklistCussWords) {
+		if (message.content.toLowerCase().includes(BlacklistCussWords[i]) && !message.member.roles.cache.some(role => role.name === 'Pure Ones')) {
+			message.reply('No, just no');
+			console.log(`'${message.member.user.tag}' said '${message.content}' and the bot found the keyword '${BlacklistCussWords[i]}'`)
 			++UsageCount
 			break
 		}
