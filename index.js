3a86430e696d7dba52abc96f1536a4fea6f66f90
TheQwertyMan <62036209+TheQwertyMan@users.noreply.github.com>
2020-05-30 19:34:48 +0200

    Made code shorter
    Shortened code by 50 lines by putting the Checking for words in a function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //nodemon --inspect index.js
+//To scrape data from 4chan i would have to go to body>form id=""delform">div class board> t[postnumber]
 
-//Require stuff
+//Require/declare stuff
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const config = require('./config.json')
@@ -19,8 +20,6 @@ const EmbedRick = new Discord.MessageEmbed()
 	
 //Blacklists
 let BlacklistLink = ["anonfiles.com/", "mega.nz/"]
-let BlacklistCussWords = ['flake nude porn', 'flake nude', 'flake porn']
-let BlacklistBadBot = ['bad bot', 'bot bad', 'stupid bot', 'complaints about bot', 'fuck the bot', 'i hate this bot', 'remove bot', 'why tf did you make this bot', 'fuck this bot']
 let BlacklistDrive = ['2tb', '2 tb', '2terabyte', '2 terabyte', 'two terabyte', 'two tb']
 let BlacklistTracker = ['new leak', 'anything new', "what's new"]
 let BlacklistSearch = ['where can i download', 'download link', 'where are those leaks', 'where can i find']
@@ -28,11 +27,9 @@ let BlacklistDontHave = ['ura zelda', 'mother 64', 'mother64', 'mario 64 beta', 
 	'mario 64 prototype', 'earthbound64', 'earthbound 64', 'mother3', 'mother 3', 'earthbound3', 'earthbound 3']
 
 //Blacklist all	
-let AllStep1 = BlacklistBadBot.concat(BlacklistDrive)
+let AllStep1 = BlacklistDontHave.concat(BlacklistDrive)
 let AllStep2 = AllStep1.concat(BlacklistTracker)
-let AllStep3 = AllStep2.concat(BlacklistSearch)
-let AllStep4 = AllStep3.concat(BlacklistDontHave)
-let All = AllStep4.concat(BlacklistCussWords)
+let All = AllStep2.concat(BlacklistSearch)
 
 //Downloads arrays
 let Downloads = ['<https://drive.google.com/open?id=14vwtcahLiRuBHzq4EOOYMmOAYn6AiHS4>', '<https://drive.google.com/open?id=1L7-T_oCnh2Nna6_MG_whkz9j1n6qrovu>', '<https://github.com/TheQwertyMan/StopItDoesntExist/archive/1.zip>']
@@ -43,15 +40,26 @@ var messageBuffer = 'TempVal'
 var UsageCount = 0
 var Blacklisted = false
 
+//Function Check
+function Check(list, sender, message) {
+	for (var i in list) {
+	if (sender.content.toLowerCase().includes(list[i])) {
+		sender.reply(message)
+		console.log(`'${sender.member.user.tag}' said '${sender.content}' and the bot found the keyword '${list[i]}'`)
+		++UsageCount
+		break
+		}
+	}
+}
+
 //On message
 client.on('message', message => {
 	
-	//Rickroll/linktest
 	if(message.content === 'testlink') {
 		message.reply(EmbedRick)
 		console.log(`'${message.member.user.tag}' Rickrolled someone`)	
 	}
-
+	
 	//Check for links
 	for (var i in BlacklistLink) {
 		if (message.content.toLowerCase().includes(BlacklistLink[i]))			{
@@ -118,7 +126,7 @@ client.on('message', message => {
 	
 	//FAQ
 	if (message.content.toLowerCase() === 'faq') {
-			message.reply(`You asked for the faq, so here it is:\n\nWhat do all the slashes mean?\npaths (/depot/depot/etc/etc) usually stands for a location in the iQue source leak, names between the slashes mean the folder that there is, in the example the location would be in the folder 'depot', then 'depot', then 'etc' and lastly 'etc'\n\nWhat does /vp/ or /ppg/ mean?\n/[letters]/ usually refers to a board on 4chan.org, this is the site that most leaks come from, /ppg/ in this case stands for 'Pokemon Prototype General', which is a kind of subboard for pokemon leaks on the /vp/ board\n\nHow do i use 4chan?\nGo to 4chan.org and select the board that you want to visit, if you are looking for leaks, the best boards are 'Video Games' (/v/), 'Pokemon' (/vp/) and 'Retro Games' (/vr/), now you can search for some keywords by pressing 'ctrl + f' or clicking on '[search]' in the top right of the site, my goto keywords are: 'leak', 'http' and 'anonfile'`)
+			message.reply(`You asked for the faq, so here it is:\n\nWhat do all the slashes mean?\npaths (/depot/depot/etc/etc) usually stands for a location in the iQue source leak, names between the slashes mean the folder that there is, in the example the location would be in the folder 'depot', then 'depot', then 'etc' and lastly 'etc'\n\nWhat does /vp/ or /ppg/ mean?\n/[letters]/ usually refers to a board on 4chan.org, this is the site that most leaks come from, /ppg/ in this case stands for 'Pokemon Prototype General', which is a kind of subboard for pokemon leaks on the /vp/ board\n\nHow do i use 4chan?\nGo to 4chan.org and select the board that you want to visit, if you are looking for leaks, the best boards are 'Video Games' (/v/), 'Pokemon' (/vp/) and 'Retro Games' (/vr/), now you can search for some keywords by pressing 'ctrl + f' or clicking on '[search]' in the top right of the site, my goto keywords are: 'leak', 'http' and 'anonfile'\n\nDo you guys have Nintendo eShop cards?\nYes, but we have to verify if you are a real person, please send your credit card details here to verify.`)
 			console.log(`${message.member.user.tag} requested the faq`)
 			++UsageCount
 	}
@@ -135,64 +143,11 @@ client.on('message', message => {
 		return
 	}
 	
-	//Blacklist filter BadBot
-	for (var i in BlacklistBadBot) {
-		if (message.content.toLowerCase().includes(BlacklistBadBot[i])) {
-			message.reply('THIS SERVER IS PRO BOT SO DONT YOU DARE INSULT IT AGAIN')
-			console.log(`'${message.member.user.tag}' said '${message.content}' and the bot found the keyword '${BlacklistBadBot[i]}'`)
-			++UsageCount
-			break
-		}
-	}
-	
-	//Blacklist filter Search
-	for (var i in BlacklistSearch) {
-		if (message.content.toLowerCase().includes(BlacklistSearch[i])) {
-			message.reply('JUST DO A GOOGLE SEARCH RETARD - TUTORIAL: <https://www.youtube.com/watch?v=R0DQfwc72PM>')
-			console.log(`'${message.member.user.tag}' said '${message.content}' and the bot found the keyword '${BlacklistSearch[i]}'`)			
-			++UsageCount
-			break
-		}
-	}
-	
-	//Blacklist filter Cusswords
-	for (var i in BlacklistCussWords) {
-		if (message.content.toLowerCase().includes(BlacklistCussWords[i])) {
-			message.reply('No, just no')
-			console.log(`'${message.member.user.tag}' said '${message.content}' and the bot found the keyword '${BlacklistCussWords[i]}'`)
-			++UsageCount
-			break
-		}
-	}
-	
-	//Blacklist filter DontHave
-	for (var i in BlacklistDontHave) {
-		if (message.content.toLowerCase().includes(BlacklistDontHave[i])) {
-			message.reply('WE DONT HAVE ANY OF THE LEAKS YOU ASK FOR - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>')
-			console.log(`'${message.member.user.tag}' said '${message.content}' and the bot found the keyword '${BlacklistDontHave[i]}'`)
-			++UsageCount
-			break
-		}
-	}
-	
-	//Blacklist filter Tracker
-	for (var i in BlacklistTracker) {
-		if (message.content.toLowerCase().includes(BlacklistTracker[i])) {
-			message.reply('THERE ARE ONLY LEAKS IF ITS ON <#708761059388948522> - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>')
-			console.log(`'${message.member.user.tag}' said '${message.content}' and the bot found the keyword '${BlacklistTracker[i]}'`)
-			++UsageCount
-			break
-		}
-	}
-
-	for (var i in BlacklistDrive) {
-		if (message.content.toLowerCase().includes(BlacklistDrive[i])) {
-			message.reply('THERE IS NO 2TB OF DATA - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>')
-			console.log(`'${message.member.user.tag}' said '${message.content}' and the bot found the keyword '${BlacklistDrive[i]}'`)
-			++UsageCount
-			break
-		}
-	}	
+	//Check in blacklists
+	Check(BlacklistDrive, message, "THERE IS NO 2TB OF DATA - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>")
+	Check(BlacklistSearch, message, "JUST DO A GOOGLE SEARCH RETARD - TUTORIAL: <https://www.youtube.com/watch?v=R0DQfwc72PM>")
+	Check(BlacklistDontHave, message, "WE DONT HAVE ANY OF THE LEAKS YOU ASK FOR - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>")
+	Check(BlacklistTracker, message, "THERE ARE ONLY LEAKS IF ITS ON <#708761059388948522> - PROOF: <https://www.youtube.com/watch?v=5AFVQI8PsQA>")	
 })
 
 client.login(config.Token)
